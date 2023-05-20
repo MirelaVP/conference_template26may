eb28c9d550dd41379ac5f8b4ac6ebbcf1c5385d8
Kyunghoon Han <kyunghoon.h@gmail.com>
2023-05-21 01:24:14 +0200

    implemented emailJS stuff in register section

--- src/pages/table.js
+++ src/pages/table.js
@@ -2,196 +2,196 @@ import React from 'react';
 import { useTable, useSortBy, useFilters, useGlobalFilter, useAsyncDebounce } from 'react-table';
 
 function GlobalFilter({
-                       preGlobalFilteredRows,
-                       globalFilter,
-                       setGlobalFilter,
-                     }) {
- const count = preGlobalFilteredRows.length
- const [value, setValue] = React.useState(globalFilter)
- const onChange = useAsyncDebounce(value => {
-   setGlobalFilter(value || undefined)
- }, 200)
+  preGlobalFilteredRows,
+  globalFilter,
+  setGlobalFilter,
+}) {
+  const count = preGlobalFilteredRows.length
+  const [value, setValue] = React.useState(globalFilter)
+  const onChange = useAsyncDebounce(value => {
+    setGlobalFilter(value || undefined)
+  }, 200)
 
- return (
-   <span>
-     Search:{' '}
-     <input
-       value={value || ""}
-       onChange={e => {
-         setValue(e.target.value);
-         onChange(e.target.value);
-       }}
-       placeholder={`${count} records...`}
-       style={{
-         fontSize: '1.1rem',
-         border: '0',
-       }}
-     />
-   </span>
- )
+  return (
+    <span>
+      Search:{' '}
+      <input
+        value={value || ""}
+        onChange={e => {
+          setValue(e.target.value);
+          onChange(e.target.value);
+        }}
+        placeholder={`${count} records...`}
+        style={{
+          fontSize: '1.1rem',
+          border: '0',
+        }}
+      />
+    </span>
+  )
 }
 
 // Define a default UI for filtering
 function DefaultColumnFilter({
-                              column: { filterValue, preFilteredRows, setFilter },
-                            }) {
- const count = preFilteredRows.length
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
 
- return (
-   <input
-     value={filterValue || ''}
-     onChange={e => {
-       setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
-     }}
-     placeholder={`Search ${count} records...`}
-   />
- )
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
 }
 
 function People() {
 
- const data = React.useMemo(
-     () => [
-       {
-         col1: 'BananaMan_1',
-         col2: 'Banana University',
-         col3: 'Works on Banana-reactor engine',
-         col4: '0.8',
-       },
-       {
-         col1: 'Monsieur Banana',
-         col2: 'Université du Banana',
-         col3: 'Le travail sur le dynamique banana-ique',
-         col4: '0.99',
-       },
-       {
-         col1: 'Eww Banana',
-         col2: 'Brahhh',
-         col3: 'Bananas are horrible: a psychological investigation',
-         col4: '0.02',
-       },
-     ],
-     []
- )
+  const data = React.useMemo(
+    () => [
+      {
+        col1: 'BananaMan_1',
+        col2: 'Banana University',
+        col3: 'Works on Banana-reactor engine',
+        col4: '0.8',
+      },
+      {
+        col1: 'Monsieur Banana',
+        col2: 'Université du Banana',
+        col3: 'Le travail sur le dynamique banana-ique',
+        col4: '0.99',
+      },
+      {
+        col1: 'Eww Banana',
+        col2: 'Brahhh',
+        col3: 'Bananas are horrible: a psychological investigation',
+        col4: '0.02',
+      },
+    ],
+    []
+  )
 
- const columns = React.useMemo(
-     () => [
-       {
-         Header: 'Participant',
-         accessor: 'col1', // accessor is the "key" in the data
-       },
-       {
-         Header: 'Affiliation',
-         accessor: 'col2',
-       },
-       {
-         Header: 'Topic',
-         accessor: 'col3',
-       },
-       {
-         Header: 'Banana-ness',
-         accessor: 'col4',
-       },
-     ],
-     []
- )
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Participant',
+        accessor: 'col1', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Affiliation',
+        accessor: 'col2',
+      },
+      {
+        Header: 'Topic',
+        accessor: 'col3',
+      },
+      {
+        Header: 'Banana-ness',
+        accessor: 'col4',
+      },
+    ],
+    []
+  )
 
- const defaultColumn = React.useMemo(
-   () => ({
-     // Let's set up our default Filter UI
-     Filter: DefaultColumnFilter,
-   }),
-   []
- )
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
 
- const {
-   getTableProps,
-   getTableBodyProps,
-   headerGroups,
-   rows,
-   prepareRow,
-   state,
-   visibleColumns,
-   preGlobalFilteredRows,
-   setGlobalFilter,
- } = useTable(
-   {
-     columns,
-     data,
-     defaultColumn, // Be sure to pass the defaultColumn option
-   },
-   useFilters,
-   useGlobalFilter,
-   useSortBy
- );
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    visibleColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn, // Be sure to pass the defaultColumn option
+    },
+    useFilters,
+    useGlobalFilter,
+    useSortBy
+  );
 
- return (
-     <div>
-       <table {...getTableProps()} style={{ border: 'solid 1px black' }}>
-         <thead>
-         {headerGroups.map(headerGroup => (
-             <tr {...headerGroup.getHeaderGroupProps()}>
-               {headerGroup.headers.map(column => (
-                   <th
-                       {...column.getHeaderProps(column.getSortByToggleProps())}
-                       style={{
-                         borderBottom: 'solid 3px red',
-                         color: 'black',
-                       }}
-                   >
-                     {column.render('Header')}
-                     <span>
-                       {column.isSorted
-                           ? column.isSortedDesc
-                               ? '🔽'
-                               : '🔼'
-                           : ''}
-                    </span>
-                    <div>{column.canFilter ? column.render('Filter') : null}</div>
-                   </th>
-               ))}
-             </tr>
-         ))}
-         <tr>
-           <th
-             colSpan={visibleColumns.length}
-             style={{
-               textAlign: 'left',
-             }}
-           >
-             <GlobalFilter
-               preGlobalFilteredRows={preGlobalFilteredRows}
-               globalFilter={state.globalFilter}
-               setGlobalFilter={setGlobalFilter}
-             />
-           </th>
-         </tr>
-         </thead>
-         <tbody {...getTableBodyProps()}>
-         {rows.map(row => {
-           prepareRow(row)
-           return (
-               <tr {...row.getRowProps()}>
-                 {row.cells.map(cell => {
-                   return (
-                       <td
-                           {...cell.getCellProps()}
-                           style={{
-                             padding: '10px',
-                             border: 'solid 1px gray',
-                           }}
-                       >
-                         {cell.render('Cell')}
-                       </td>
-                   )
-                 })}
-               </tr>
-           )
-         })}
-         </tbody>
-       </table>
-     </div>
- );
+  return (
+    <div>
+      <table {...getTableProps()} style={{ border: 'solid 1px black' }}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  style={{
+                    borderBottom: 'solid 3px red',
+                    color: 'black',
+                  }}
+                >
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? '🔽'
+                        : '🔼'
+                      : ''}
+                  </span>
+                  <div>{column.canFilter ? column.render('Filter') : null}</div>
+                </th>
+              ))}
+            </tr>
+          ))}
+          <tr>
+            <th
+              colSpan={visibleColumns.length}
+              style={{
+                textAlign: 'left',
+              }}
+            >
+              <GlobalFilter
+                preGlobalFilteredRows={preGlobalFilteredRows}
+                globalFilter={state.globalFilter}
+                setGlobalFilter={setGlobalFilter}
+              />
+            </th>
+          </tr>
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        padding: '10px',
+                        border: 'solid 1px gray',
+                      }}
+                    >
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default People;
